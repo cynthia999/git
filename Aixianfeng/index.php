027fd305d99e9bae9e8bc92b15d4491ab2f8cea3
zhangyuanyuan <2604608139@qq.com>
2016-12-20 10:21:22 +0800

    提交

--- Aixianfeng/index.php
+++ Aixianfeng/index.php
@@ -73,8 +73,10 @@ $signPackage = $jssdk->GetSignPackage();
 			}
 		</style>
 	</head>
-	<body>		
-			<div class="main_a">
+	<body>	
+			<button onclick="getLocation()">获取地理位置</button>
+			<button onclick="getPosition()">查看位置</button>	
+			<!-- <div class="main_a">
 				<div class = "home_a"></div>
 				<div class = "market_a"></div>
 				<div class = "order_a"></div>
@@ -99,7 +101,7 @@ $signPackage = $jssdk->GetSignPackage();
 				<img src="img/f4.png" alt="" />
 				<p><a href="#mine_a">我的</a></p>
 			</figure>
-		</div>
+					</div> -->
 	</body>
 	<!--<script type="text/javascript">
 		(function(window){
@@ -112,6 +114,8 @@ $signPackage = $jssdk->GetSignPackage();
 		})(window);
 	</script>-->
 	<script>
+
+
 		wx.config({
     debug: true,
     appId: '<?php echo $signPackage["appId"];?>',
@@ -155,14 +159,33 @@ $signPackage = $jssdk->GetSignPackage();
 
      ]
   });
+
+
+
+function getLocation(){
 	wx.getLocation({
     type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
     success: function (res) {
-        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+         latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+         longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
         var speed = res.speed; // 速度，以米/每秒计
-        var accuracy = res.accuracy; // 位置精度
+        var accuracy = res.accuracy; // 位置精度       
+        alert('经度'+longitude);
+        alert('纬度'+latitude);
     }
 });
+}
+	var latitude=0;
+	var longitude=0;
+	function getPosition(){
+		wx.openLocation({
+    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+    name: '', // 位置名
+    address: '', // 地址详情说明
+    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+});
+	}
 	</script>
 </html>
